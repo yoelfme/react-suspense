b631c651210785483edf56bab2d09d4f5a4daf75
Yoel Monzon <yoelfme@gmail.com>
2022-02-01 21:02:53 -0600

    [update] use the context with a context provider

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -29,23 +29,32 @@ const SUSPENSE_CONFIG = {
   busyMinDurationMs: 700,
 }
 
-const getPokemonResource = (pokemonName) => {
-  const lowerName = pokemonName.toLowerCase()
-  let resource = pokemonResourceCache[lowerName]
 
-  if (!resource) {
-    resource = createPokemonResource(lowerName)
-    pokemonResourceCache[lowerName] = resource
-  }
-
-  return resource
-}
-
-const pokemonResourceCache = {}
-const PokemonResourceCacheContext = React.createContext(getPokemonResource)
+const PokemonResourceCacheContext = React.createContext()
 
 const usePokemonResourceCache = () => {
   return React.useContext(PokemonResourceCacheContext)
+}
+
+function PokemonCacheProvider({ children }) {
+  const cache = React.useRef({})
+  const getPokemonResource = React.useCallback((pokemonName) => {
+    const lowerName = pokemonName.toLowerCase()
+    let resource = cache.current[lowerName]
+
+    if (!resource) {
+      resource = createPokemonResource(lowerName)
+      cache.current[lowerName] = resource
+    }
+
+    return resource
+  }, [])
+
+  return (
+    <PokemonResourceCacheContext.Provider value={getPokemonResource}>
+      {children}
+    </PokemonResourceCacheContext.Provider>
+  )
 }
 
 function createPokemonResource(pokemonName) {
@@ -100,4 +109,12 @@ function App() {
   )
 }
 
-export default App
+function AppWithProvider() {
+  return (
+    <PokemonCacheProvider>
+      <App />
+    </PokemonCacheProvider>
+  )
+}
+
+export default AppWithProvider
